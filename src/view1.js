cee6a16e488c16edee1c01c47c37669460cc0f49
xXSnehalXx <snehalmachan@gmail.com>
2019-02-27 20:51:05 +0530

    successful navigation

--- src/view1.js
+++ src/view1.js
@@ -7,30 +7,98 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 import React from "react";
-import { View, Text ,Button} from "react-native";
+import { View, Text ,Button , Platform ,Image} from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 class HomeScreen extends React.Component {
+    static navigationOptions = {
+    title: 'Home',
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+    headerRight: (
+      <Button
+        onPress={() => alert('This is a button!')}
+        title="INFO"
+        color={Platform.OS=="ios"?"#FFFFFF":"#f4511e"}
+
+      />
+    ),
+  };
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
         <Button
           title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
+          onPress={() => {
+            /* 1. Navigate to the Details route with params */
+            this.props.navigation.navigate('Details',{
+                itemId : 84
+            })
+          }}
         />
       </View>
     );
   }
 }
 class DetailsScreen extends React.Component {
+  //   static navigationOptions = ({ navigation }) => {
+  //   return {
+  //     title: JSON.stringify(navigation.getParam('itemId', 'No id')),
+  //   };
+  // };
+  static navigationOptions = ({ navigation}) => {
+    return {
+      headerTitle: <LogoTitle />,
+      headerRight: (
+        <Button
+          onPress={navigation.getParam('increaseCount')}
+          title={`${JSON.stringify(navigation.getParam('increaseCount'))}`}
+          color="#fff"
+        />
+      ),
+    };
+  };
+  constructor(props){
+      super(props);
+      this.state={
+          count:0
+      };
+  }
+  componentDidMount() {
+    this.props.navigation.setParams({ increaseCount: this._increaseCount });
+  }
+
+
+  _increaseCount = () => {
+    this.setState((state)=>{ count:state.count + 1 });
+  };
+  //every navigated or pushed view comes with a back butotn on left , if i dont want
+  //i can use headerLeft:null in navigationOptions
   render() {
+    /* 2. Get the param, provide a fallback value if not available */
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const otherParam = navigation.getParam('otherParam', 'some default value');
+    const paramList = this.props.navigation.state.params;
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+        <Text>{JSON.stringify(paramList)?JSON.stringify(paramList):"Nothing here"}</Text>
         <Button
           title="Go to Details... again"
-          onPress={() => this.props.navigation.push('Details')}
+          onPress={() =>
+            this.props.navigation.push('Details', {
+              itemId: Math.floor(Math.random() * 100),
+            })}
         />
         <Button
           title="Go to Home"
@@ -40,22 +108,43 @@ class DetailsScreen extends React.Component {
           title="Go back"
           onPress={() => this.props.navigation.goBack()}
         />
-        <Button
-          title="Pop to top"
-          onPress={() => this.props.navigation.popToTop()}
-        />
       </View>
     );
   }
 }
-
+class LogoTitle extends React.Component {
+  render() {
+    return (
+        <Image
+    source={{
+      uri: 'https://facebook.github.io/react/logo-og.png',
+      method: 'POST',
+      headers: {
+        Pragma: 'no-cache',
+      },
+      body: 'Your Body goes here',
+    }}
+    style={{width: 30, height: 30}}
+  />
+    );
+  }
+}
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
     Details: DetailsScreen
   },
   {
-    initialRouteName: "Home"
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
   }
 );
 
